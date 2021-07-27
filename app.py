9aa012647ffff9eccbc008c89f22055e93e9095b
Rajendra <rajendradanve@gmail.com>
2021-07-27 14:53:17 +0000

    index.html and base.html file created with basic html

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import os
-from flask import Flask
-
+from flask import (
+    Flask, flash, render_template,  
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DB_NAME"]= os.environ.get("MONGO_DB_NAME")
+app.config["MONGO_URI"]= os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route("/")
+@app.route("/index")
 def index():
-    return "hello world"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
