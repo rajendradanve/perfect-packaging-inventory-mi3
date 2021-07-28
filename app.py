50c9be66d3964816b67199414d605fa3adc87ff4
Rajendra <rajendradanve@gmail.com>
2021-07-28 21:38:08 +0000

    admin and customer pages created

--- app.py
+++ app.py
@@ -18,11 +18,37 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/login", methods=["GET", "POST"])
-def login():
+@app.route("/index")
+def index():
     
     return render_template("index.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+    
+        if existing_user:
+            # Check if password provided by existing user is correct
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                if session["user"] == "admin":
+                    return redirect(url_for(
+                        "admin", username=session["user"]))
+                else:
+                    return redirect(url_for(
+                        "customer", username=session["user"]))
+            else:
+                # invalid password
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
